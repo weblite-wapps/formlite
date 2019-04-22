7c5ab4dc23f74168fcb0d653a80f1611d0af07ec
javad <vahedi.r46@gmail.com>
2019-04-22 09:36:37 -0700

    fix(excel): temp commit

--- frontend/src/main/components/Header.vue
+++ frontend/src/main/components/Header.vue
@@ -7,7 +7,7 @@
       @click="switchState('reviewing')"
     >reply</i>
 
-    <downloadexcel :data="json_data" :fields="json_fields" name="form.csv" type="csv">
+    <downloadexcel :data="json_data" :fields="json_fields" name="form.xls" type="xls">
       <i :class="$style['back-icon']" v-if="state == 'reviewing' &&  creator">save</i>
     </downloadexcel>
 
@@ -22,7 +22,7 @@
 
 <script>
 import downloadexcel from 'vue-json-excel'
-
+const { R } = window
 export default {
   name: 'Header',
 
@@ -42,13 +42,14 @@ export default {
   data: () => ({
     json_fields: {
       Name: 'username',
-      Question1: 'answers1',
+      Question1: 'answers1'[0],
+      Question2: 'answers1'[1],
     },
 
     json_data: [
       {
         username: 'Tony Pena',
-        answers1: [11, 22],
+        answers1: ['1', '2'],
         // answers2: '22',
         // answers3: '33',
       },
@@ -60,9 +61,15 @@ export default {
       // },
     ],
   }),
+  methods:{
+    makeObject = (obj) => {
+      const {username, answers} = obj
+      const flattenObj = 
+    }
+  },
   computed: {
     jsf() {
-      return this.Q
+      return R.map(makeObject, object)
     },
   },
   updated() {
